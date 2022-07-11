cd6d9afab58efaf38593ec74adea8a4919192f1d
Quentin Dufournet <qdu.dev.pro@gmail.com>
2022-07-11 22:33:02 +0200

    fix tests + add workaround local tokens file

--- test_madame_monsieur.py
+++ test_madame_monsieur.py
@@ -21,9 +21,16 @@ def load_tokens():
     :return: the discordWebhook, jokeApi, and newsApi variables.
     """
     
-    BLAGUE_API_KEY = os.environ['BLAGUE_API_KEY']
-    DISCORD_WEBHOOK_URL = os.environ['DISCORD_WEBHOOK_URL']
-    NEWS_API_KEY = os.environ['NEWS_API_KEY']
+    try:
+        with open('tokens', 'r') as f:
+            f = f.readlines()
+            DISCORD_WEBHOOK_URL = f[0].split('=')[1].strip('\n')
+            BLAGUE_API_KEY = f[1].split('=')[1].strip('\n')
+            NEWS_API_KEY = f[2].split('=')[1].strip('\n')
+    except Exception as e:
+        BLAGUE_API_KEY = os.environ['BLAGUE_API_KEY']
+        DISCORD_WEBHOOK_URL = os.environ['DISCORD_WEBHOOK_URL']
+        NEWS_API_KEY = os.environ['NEWS_API_KEY']
     
     return DISCORD_WEBHOOK_URL, BLAGUE_API_KEY, NEWS_API_KEY if '' not in [DISCORD_WEBHOOK_URL, BLAGUE_API_KEY, NEWS_API_KEY] else None
 
@@ -65,4 +72,4 @@ def test_send_info():
     * Send a request to the Discord webhook URL with the info as an embed
     * Check the response's status code
     """
-    assert script.send_info(load_tokens()[0], 'TEST INFO', 'https://www.google.fr', 'https://www.google.fr/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png') == 204
+    assert script.send_info(load_tokens()[0], 'TEST INFO', 'https://www.github.com', 'https://github.githubassets.com/app-icon-192.png') == 204
