49a0497074a8c6192f7fd06a9b9149464b21d536
Quentin "MCXIV" Dufournet <43221669+MCXIV@users.noreply.github.com>
2022-07-11 23:58:10 +0200

    -> class + fix tests + doc send_info (#2)
    * -> class + fix tests + doc send_info
    
    * fix double try except
    
    * 2nd fix double try except + fix tests (TMR error)

--- madame_monsieur.py
+++ madame_monsieur.py
@@ -11,6 +11,7 @@ import json
 import time
 import logging
 import sys
+import os
 
 # 3rd party
 import requests
@@ -21,139 +22,146 @@ DEBUG = 0
 
 # --------------------------------------------------
 
-def send_meteo(webhook_url, ville):
-    """
-    It sends a request to the Discord webhook URL with the meteo image as an embed
-    
-    :param webhook_url: The URL of the webhook you want to send the message to
-    :param ville: The city you want to get the weather for
-    :return: The response's status code from the POST request.
-    """
-    
-    embed = {
-        'title': 'Bonjour, c\'est la météo', 
-        'url': f'https://wttr.in/{ville}.png?qp1m', 
-        'image': {
-            'url': f'https://wttr.in/{ville}.png?qp1m&lang=fr'
-            }
-        }
-    
-
-    data = {
-        'content': '',
-        'embeds': [embed]
-    }
-
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    return requests.post(webhook_url, data=json.dumps(data).encode('utf-8'), headers=headers).status_code
-
-def send_joke(webhook_url, jokeApi, type):
-    """
-    It sends a joke to a Discord webhook
-    
-    :param webhook_url: The webhook URL you copied from the Discord channel
-    :param jokeApi: The API key for the joke API
-    :param type: The type of joke you want to send
-    :return: The response's status code from the POST request.
-    """
-    
-    blague = requests.get(f'https://www.blagues-api.fr/api/type/{type}/random', headers={'Authorization': f'Bearer {jokeApi}'}).json()
-
-    embed = {
-        'title': 'Bonjour, c\'est la blague',
-        'description': blague['joke'] + ' ' + blague['answer'],
-    }
-    
-    data = {
-        'content': '',
-        'embeds': [embed]
-    }
-    
-    headers = {
-        'Content-Type': 'application/json'
-    }
+class MadameMonsieur:
+    def __init__(self):
+        """
+        It reads the tokens from the file and stores them in variables.
+        """
         
-    return requests.post(webhook_url, data=json.dumps(data).encode('utf-8'), headers=headers).status_code
-
-def send_fact(webhook_url, language='fr'):
-    """
-    It gets a random fact from a website, translates it to the language specified, and sends it to the
-    webhook URL
-    
-    :param webhook_url: The webhook URL you got from the Discord server
-    :param language: The language you want the fact to be translated to
-    :return: The response's status code from the POST request.
-    """
-    
-    fact = requests.get('https://fungenerators.com/random/facts/').text
-    fact = GoogleTranslator(source='english', target=language).translate(fact.split('<h2 class="wow fadeInUp animated"  data-wow-delay=".6s">')[1].split('<span class="text-muted">')[0])
-    embed = {
-        'title': 'Bonjour, c\'est le fact',
-        'description': fact,
-    }
-    
-    data = {
-        'content': '',
-        'embeds': [embed]
-    }
-    
-    headers = {
-        'Content-Type': 'application/json'
-    }
+        # Set logger
+        logging.basicConfig(filename='madame_monsieur.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+        logging.info('Starting Madame Monsieur')
+        # Try to load tokens
+        try:
+            with open('tokens', 'r') as f:
+                f = f.readlines()
+                self.DISCORD_WEBHOOK_URL = f[0].split('=')[1].strip('\n')
+                self.BLAGUE_API_KEY = f[1].split('=')[1].strip('\n')
+                self.NEWS_API_KEY = f[2].split('=')[1].strip('\n')
+                return None
+        except Exception as e:
+            logging.error(f'Could not load tokens: {e}')
         
-    return requests.post(webhook_url, data=json.dumps(data).encode('utf-8'), headers=headers).status_code
-
-def send_news(webhook_url, newsApi):
-    """
-    It sends news to a Discord channel using a webhook
+        try:
+            self.BLAGUE_API_KEY = os.environ['BLAGUE_API_KEY']
+            self.DISCORD_WEBHOOK_URL = os.environ['DISCORD_WEBHOOK_URL']
+            self.NEWS_API_KEY = os.environ['NEWS_API_KEY']
+        except Exception as e:
+            logging.error(f'Could not load tokens: {e}')
+            sys.exit(1)
     
-    :param webhook_url: The URL of the webhook you want to send the message to
-    :param newsApi: The API key for the news API
-    :return: A list of response code.
-    """
-    
-    url = "https://google-news1.p.rapidapi.com/topic-headlines"
-
-    querystring = {"topic":"TECHNOLOGY","country":"FR","lang":"fr","limit":"5"}
-
-    headers = {
-        "X-RapidAPI-Key": newsApi,
-        "X-RapidAPI-Host": "google-news1.p.rapidapi.com"
-    }
-
-    news = json.loads(requests.request("GET", url, headers=headers, params=querystring).text)
-    
-    responseCode = []
-    
-    timeOfDay = 'du DEBUG'
-    if time.localtime().tm_hour == 8:
-        timeOfDay = 'du matin!'
-    elif time.localtime().tm_hour == 13:
-        timeOfDay = 'de l\'après-midi!'
-    elif time.localtime().tm_hour == 18:
-        timeOfDay = 'du soir!'    
-    
-    embed = {
-        'title': 'Bonjour, c\'est les actus ' + timeOfDay,
-        'description': 'Les 5 dernières actualités tech. du moment.',
-        'url': 'https://rapidapi.com/ubillarnet/api/google-news1/details',
-    }
-    data = {
-        'content': '',
-        'embeds': [embed]
-    }
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    responseCode.append(requests.post(webhook_url, data=json.dumps(data).encode('utf-8'), headers=headers).status_code)
-    
-    for i in news['articles']:
+    def send_meteo(self, ville):
+        """
+        It sends a request to the Discord webhook URL with the meteo image as an embed
+        
+        :param ville: The city you want to get the weather for
+        :return: The response's status code from the POST request.
+        """
+        
         embed = {
-            'title': i['title'],
-            'url': i['link'],
+            'title': 'Bonjour, c\'est la météo', 
+            'url': f'https://wttr.in/{ville}.png?qp1m', 
+            'image': {
+                'url': f'https://wttr.in/{ville}.png?qp1m&lang=fr'
+                }
+            }
+        
+
+        data = {
+            'content': '',
+            'embeds': [embed]
+        }
+
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        return requests.post(self.DISCORD_WEBHOOK_URL, data=json.dumps(data).encode('utf-8'), headers=headers).status_code
+
+    def send_joke(self, type):
+        """
+        It sends a joke to a Discord webhook
+        
+        :param type: The type of joke you want to send
+        :return: The response's status code from the POST request.
+        """
+        
+        blague = requests.get(f'https://www.blagues-api.fr/api/type/{type}/random', headers={'Authorization': f'Bearer {self.BLAGUE_API_KEY}'}).json()
+
+        embed = {
+            'title': 'Bonjour, c\'est la blague',
+            'description': blague['joke'] + ' ' + blague['answer'],
+        }
+        
+        data = {
+            'content': '',
+            'embeds': [embed]
+        }
+        
+        headers = {
+            'Content-Type': 'application/json'
+        }
+            
+        return requests.post(self.DISCORD_WEBHOOK_URL, data=json.dumps(data).encode('utf-8'), headers=headers).status_code
+
+    def send_fact(self, language='fr'):
+        """
+        It gets a random fact from a website, translates it to the language specified, and sends it to the
+        webhook URL
+
+        :param language: The language you want the fact to be translated to
+        :return: The response's status code from the POST request.
+        """
+        
+        fact = requests.get('https://fungenerators.com/random/facts/').text
+        fact = GoogleTranslator(source='english', target=language).translate(fact.split('<h2 class="wow fadeInUp animated"  data-wow-delay=".6s">')[1].split('<span class="text-muted">')[0])
+        embed = {
+            'title': 'Bonjour, c\'est le fact',
+            'description': fact,
+        }
+        
+        data = {
+            'content': '',
+            'embeds': [embed]
+        }
+        
+        headers = {
+            'Content-Type': 'application/json'
+        }
+            
+        return requests.post(self.DISCORD_WEBHOOK_URL, data=json.dumps(data).encode('utf-8'), headers=headers).status_code
+
+    def send_news(self):
+        """
+        It sends news to a Discord channel using a webhook
+
+        :param newsApi: The API key for the news API
+        :return: A list of response code.
+        """
+        
+        url = "https://google-news1.p.rapidapi.com/topic-headlines"
+
+        querystring = {"topic":"TECHNOLOGY","country":"FR","lang":"fr","limit":"5"}
+
+        headers = {
+            "X-RapidAPI-Key": self.NEWS_API_KEY,
+            "X-RapidAPI-Host": "google-news1.p.rapidapi.com"
+        }
+
+        news = json.loads(requests.request("GET", url, headers=headers, params=querystring).text)
+
+        timeOfDay = 'du DEBUG'
+        if time.localtime().tm_hour == 8:
+            timeOfDay = 'du matin!'
+        elif time.localtime().tm_hour == 13:
+            timeOfDay = 'de l\'aprÃ¨s-midi!'
+        elif time.localtime().tm_hour == 18:
+            timeOfDay = 'du soir!'    
+
+        embed = {
+            'title': 'Bonjour, c\'est les actus ' + timeOfDay,
+            'description': 'Les 5 derniÃ¨res actualitÃ©s tech. du moment.',
+            'url': 'https://rapidapi.com/ubillarnet/api/google-news1/details',
         }
         data = {
             'content': '',
@@ -162,42 +170,55 @@ def send_news(webhook_url, newsApi):
         headers = {
             'Content-Type': 'application/json'
         }
-        responseCode.append(requests.post(webhook_url, data=json.dumps(data).encode('utf-8'), headers=headers).status_code)
+        responseCode = [requests.post(self.DISCORD_WEBHOOK_URL, data=json.dumps(data).encode('utf-8'), headers=headers).status_code]
 
-    return responseCode
-    
-def send_info(webhook_url, info, infoUrl, imageUrl):
-    embed = {
-        'title': 'Bonjour, c\'est une info',
-        'description': info,
-        'url': infoUrl,
-        'image' : {'url' : imageUrl},
-    }
-    
-    data = {
-        'content': '',
-        'embeds': [embed]
-    }
-    
-    headers = {
-        'Content-Type': 'application/json'
-    }
+        for i in news['articles']:
+            embed = {
+                'title': i['title'],
+                'url': i['link'],
+            }
+            data = {
+                'content': '',
+                'embeds': [embed]
+            }
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            responseCode.append(requests.post(self.DISCORD_WEBHOOK_URL, data=json.dumps(data).encode('utf-8'), headers=headers).status_code)
+
+        return responseCode
         
-    return requests.post(webhook_url, data=json.dumps(data).encode('utf-8'), headers=headers).status_code
-    
+    def send_info(self, info, infoUrl, imageUrl):
+        """
+        It sends a message to a Discord webhook
+        
+        :param info: The text that will be displayed in the embed
+        :param infoUrl: The URL of the info
+        :param imageUrl: The URL of the image you want to send
+        :return: The status code of the request.
+        """
+        
+        embed = {
+            'title': 'Bonjour, c\'est une info',
+            'description': info,
+            'url': infoUrl,
+            'image' : {'url' : imageUrl},
+        }
+        
+        data = {
+            'content': '',
+            'embeds': [embed]
+        }
+        
+        headers = {
+            'Content-Type': 'application/json'
+        }
+            
+        return requests.post(self.DISCORD_WEBHOOK_URL, data=json.dumps(data).encode('utf-8'), headers=headers).status_code
+        
+        
 if __name__ == '__main__':
-    logging.basicConfig(filename='madame_monsieur.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    logging.info('Starting Madame Monsieur')
-    try:
-        with open('tokens', 'r') as f:
-            tokens = f.readlines()
-            discordWebhook = tokens[0].split('=')[1].strip('\n')
-            jokeApi = tokens[1].split('=')[1].strip('\n')
-            newsApi = tokens[2].split('=')[1].strip('\n')
-            logging.info('Tokens loaded')
-    except Exception as e:
-        logging.error('Error while reading tokens')
-        sys.exit(e)
+    mM = MadameMonsieur()
 
     flag_meteo = 1
     flag_joke = 1
@@ -206,17 +227,17 @@ if __name__ == '__main__':
 
     while 1:
         if time.localtime().tm_hour == 8 and time.localtime().tm_min == 0 and flag_meteo == 1 or DEBUG:
-            logging.info('Sent meteo - ' + str(send_meteo(discordWebhook, 'Lyon')))
-            logging.info('Sent meteo - ' + str(send_meteo(discordWebhook, 'Oyonnax')))
+            logging.info('Sent meteo - ' + str(mM.send_meteo('Lyon')))
+            logging.info('Sent meteo - ' + str(mM.send_meteo('Oyonnax')))
             flag_meteo = time.time()
         if time.localtime().tm_hour in [9, 11, 13, 15, 17, 19, 21] and time.localtime().tm_min == 0 and flag_joke == 1 or DEBUG:
-            logging.info('Sent joke - ' + str(send_joke(discordWebhook, jokeApi, 'dark')))
+            logging.info('Sent joke - ' + str(mM.send_joke('dark')))
             flag_joke = time.time()
         if time.localtime().tm_hour in [10, 12, 14, 16, 18, 20, 22] and time.localtime().tm_min == 0 and flag_fact == 1 or DEBUG:
-            logging.info('Sent fact - ' +  str(send_fact(discordWebhook, 'fr')))
+            logging.info('Sent fact - ' +  str(mM.send_fact()))
             flag_fact = time.time()
         if time.localtime().tm_hour in [8, 13, 18] and time.localtime().tm_min == 0 and flag_news == 1 or DEBUG:    
-            logging.info('Sent news - ' + str(send_news(discordWebhook, newsApi)))
+            logging.info('Sent news - ' + str(mM.send_news()))
             flag_news = time.time()
 
         if time.time() - flag_meteo >= 86400 and flag_meteo != 1:
