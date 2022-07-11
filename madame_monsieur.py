4b7741146df2e982a7c2f17ee12b753e3b5a3e7e
Quentin Dufournet <qdu.dev.pro@gmail.com>
2022-07-12 00:08:19 +0200

    add comments

--- madame_monsieur.py
+++ madame_monsieur.py
@@ -41,7 +41,7 @@ class MadameMonsieur:
                 return None
         except Exception as e:
             logging.error(f'Could not load tokens: {e}')
-        
+        # If tokens file failed, try to load tokens from environment variables (for Github Actions)
         try:
             self.BLAGUE_API_KEY = os.environ['BLAGUE_API_KEY']
             self.DISCORD_WEBHOOK_URL = os.environ['DISCORD_WEBHOOK_URL']
@@ -66,7 +66,6 @@ class MadameMonsieur:
                 }
             }
         
-
         data = {
             'content': '',
             'embeds': [embed]
@@ -220,22 +219,27 @@ class MadameMonsieur:
 if __name__ == '__main__':
     mM = MadameMonsieur()
 
+    # Flags for the different types of messages
     flag_meteo = 1
     flag_joke = 1
     flag_fact = 1
     flag_news = 1
 
     while 1:
+        # If the time is 8:00, send the meteo
         if time.localtime().tm_hour == 8 and time.localtime().tm_min == 0 and flag_meteo == 1 or DEBUG:
             logging.info('Sent meteo - ' + str(mM.send_meteo('Lyon')))
             logging.info('Sent meteo - ' + str(mM.send_meteo('Oyonnax')))
             flag_meteo = time.time()
+        # If the hour is even (from 9AM to 9PM), send the joke
         if time.localtime().tm_hour in [9, 11, 13, 15, 17, 19, 21] and time.localtime().tm_min == 0 and flag_joke == 1 or DEBUG:
             logging.info('Sent joke - ' + str(mM.send_joke('dark')))
             flag_joke = time.time()
+        # If the hour is odd (from 10AM to 10PM), send a fact
         if time.localtime().tm_hour in [10, 12, 14, 16, 18, 20, 22] and time.localtime().tm_min == 0 and flag_fact == 1 or DEBUG:
             logging.info('Sent fact - ' +  str(mM.send_fact()))
             flag_fact = time.time()
+        # If the hour is 8AM, or 1PM, or 6PM, send the news
         if time.localtime().tm_hour in [8, 13, 18] and time.localtime().tm_min == 0 and flag_news == 1 or DEBUG:    
             logging.info('Sent news - ' + str(mM.send_news()))
             flag_news = time.time()
